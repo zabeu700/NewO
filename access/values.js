77c1d0b7ec6d58a0cefc11331eb44e9a158ede87
zabeu700 <105270553+zabeu700@users.noreply.github.com>
2023-09-26 17:03:30 +0300

    values.js

--- access/values.js
+++ access/values.js
@@ -14,11 +14,11 @@ var passport_id = "3872011238" // Номер паспорта
 var kpp_id = "3872011238" // ИНН
 
 // Права
-var rights_valid_until = "11.09.2023" // Права "Дійсні до"
+var rights_valid_until = "11.09.2026" // Права "Дійсні до"
 var rights_categories = "A, B" // Права "Категорії"
 var rights_tsc = "ТСЦ 8631" // Права "Видав
 var rights_id = "KBE968639" // Права "Серія та номер""
 
 // COVID-сертификат
-var covid_valid_until = "09.02.2023" // Сертификат "Дійсний до"
+var covid_valid_until = "09.02.2024" // Сертификат "Дійсний до"
 var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
